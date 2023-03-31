65d7073ac64bf78bdc8dff11261fa8ea17257ace
tahmid <gaziur.tahmid@gmail.com>
2023-04-01 00:20:29 +0600

    ritrive add quantity

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Productitems from '../ProductItems/Productitems';
 import './Shop.css';
 
 const Shop = () => {
    const [products, setProduct] = useState([]);
-   const [cart, setcart] = useState([]);
+   const [cart, setCart] = useState([]);
 
    useEffect(() => {
       fetch('products.json')
          .then(res => res.json())
          .then(data => setProduct(data))
    },[])
+
+   useEffect(()=>{
+     
+    const storedCart = getShoppingCart();
+    const saveCart = [];
+      for(const id in storedCart){
+         // console.log(id)
+         const addProduct = products.find(product => product.id === id);
+         if(addProduct){
+            const quantity = storedCart[id];
+            addProduct.quantity = quantity;
+            
+            saveCart.push(addProduct)
+
+         }
+         console.log(addProduct)
+      }
+      setCart(saveCart);
+   },[products])
    const hendelAddToCard = (product) => {
-      const newcart =[...cart, product];
-      setcart(newcart);
+      const newCart =[...cart, product];
+      setCart(newCart);
+      addToDb(product.id)
    }
    return (
       <nav className='shop-container'>
